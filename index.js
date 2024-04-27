6bbfccd1ead1d8990c5f7faef5b58201658d8e87
vatiza <rakibur.mac@gmail.com>
2024-04-27 16:22:16 +0600

    delte data form ui

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
@@ -30,11 +30,19 @@ async function run() {
     app.post("/chocolate", async (req, res) => {
       const newChocolate = req.body;
       console.log(newChocolate);
+      const result = await chocolateCollection.insertOne(newChocolate);
+      res.send(result);
     });
 
     app.get("/chocolate", async (req, res) => {
       const cursor = chocolateCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.delete("/chocolate/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await chocolateCollection.deleteOne(query);
       res.send(result);
     });
 
